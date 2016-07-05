f95e1d5811c178ab6544a915aa3bd8e0af2e59e4
kapibara92 <asia_9215@onet.eu>
2016-07-05 15:50:26 +0200

    gra FizzBuzz

---
+++ FizzBuzzLibrary/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzz
+    {
+        public static string StartGame()
+        {
+            
+            string test="";
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i%3==0 && i % 5 == 0)
+                {
+                    test += "FizzBuzz, ";
+                }
+                else if (i % 3 == 0)
+                {
+                    test += "Fizz, ";
+                }
+                else if (i % 5 == 0)
+                {
+                    test += "Buzz, ";
+                }
+                else
+                {
+                    test += i.ToString()+", ";
+                }
+            }
+
+            return test;
+        }
+    }
+}
---
+++ FizzBuzzLibraryTest/FizzBuzzTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzLibrary;
+
+
+namespace FizzBuzzLibraryTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        private string checkGame;
+        [TestMethod]
+        public void CheckMultipleByThree()
+        {
+            checkGame = FizzBuzz.StartGame();
+            Assert.IsTrue(checkGame.Contains("Fizz"));
+        }
+        [TestMethod]
+        public void CheckMultipleByFive()
+        {
+            checkGame = FizzBuzz.StartGame();
+            Assert.IsTrue(checkGame.Contains("Buzz"));
+        }
+        [TestMethod]
+        public void CheckMultipleByThreeAndFive()
+        {
+            checkGame = FizzBuzz.StartGame();
+            Assert.IsTrue(checkGame.Contains("FizzBuzz"));
+        }
+
+    }
+}
